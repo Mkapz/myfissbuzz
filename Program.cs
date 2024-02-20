ce241b4545f035cfa0eb290919e7392b37af4849
Michael Kapronica <mkap25@yahoo.com>
2024-02-20 17:47:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Fizzbuss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int j= 5; j <= 100; j++)
+            {
+                string results = "";
+                if (j % 3 == 0) results = "Fizz";
+
+                if (j % 5 == 0) results = results + "Buzz";
+
+                if (results.Length == 0) results = j.ToString();
+                Console.WriteLine(results);
+
+
+            }
+
+        }
+    }
+}
